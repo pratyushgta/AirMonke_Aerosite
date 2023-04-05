121f4a177a214fe125c144f8c747edeaf4b85407
Pratyush Kumar <pratyushgta@gmail.com>
2023-04-05 21:13:13 +0530

    Update #05-04-2023

--- signup.php
+++ signup.php
@@ -53,25 +53,69 @@ mysqli_close($conn);
 
 <body>
     <h1>Sign Up!</h1>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" name="registerForm"
-          onsubmit="return validateRegisterForm()">
-        <label for="First Name">First Name:</label>
-        <input type="text" id="f_name" name="f_name" required>
-        <br>
-        <label for="Last Name">Last Name:</label>
-        <input type="text" id="l_name" name="l_name" required>
-        <br>
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required>
-        <br>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
-        <br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
-        <br>
-        <input type="submit" value="Submit">
-    </form>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" name="registerForm"
+      ng-app="registerApp" ng-controller="registerController" novalidate>
+    <label for="First Name">First Name:</label>
+    <input type="text" id="f_name" name="f_name" ng-model="formData.f_name" ng-pattern="/^[a-zA-Z]+$/" required>
+    <div ng-show="registerForm.f_name.$touched && registerForm.f_name.$error.required">
+        Hmm are you nameless?
+    </div>
+    <div ng-show="registerForm.f_name.$error.pattern">
+        Whattt? Your name has numbers in it? That's a first
+    </div>
+    <br>
+
+    <label for="Last Name">Last Name:</label>
+    <input type="text" id="l_name" name="l_name" ng-model="formData.l_name" ng-pattern="/^[a-zA-Z]+$/" required>
+    <div ng-show="registerForm.l_name.$touched && registerForm.l_name.$error.required">
+        Last name is required!
+    </div>
+    <div ng-show="registerForm.l_name.$error.pattern">
+        Again, last name can contain alphabets only!
+    </div>
+    <br>
+
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" ng-model="formData.username" ng-minlength="5" required>
+    <div ng-show="registerForm.username.$touched && registerForm.username.$error.required">
+        Please enter an username! It is used as a unique customer id!
+    </div>
+    <div ng-show="registerForm.username.$error.minlength">
+        Username should be at least 5 characters long!
+    </div>
+    <br>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" ng-model="formData.email" required>
+    <div ng-show="registerForm.email.$touched && registerForm.email.$error.required">
+        Email is required... How are we gonna span you then?
+    </div>
+    <div ng-show="registerForm.email.$error.email">
+        Please enter a valid email!
+    </div>
+    <br>
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" ng-model="formData.password"
+           ng-pattern="/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#\$%\^&\*])(?=.{5,})/" required>
+    <div ng-show="registerForm.password.$touched && registerForm.password.$error.required">
+        Password is required!
+    </div>
+    <div ng-show="registerForm.password.$error.pattern">
+        Password should be at least 5 characters long and must contain at least one uppercase letter, one special character, and one number.
+    </div>
+    <br>
+
+    <input type="submit" value="Submit" ng-disabled="registerForm.$invalid">
+</form>
+
+<script>
+    var app = angular.module("registerApp", []);
+    app.controller("registerController", function ($scope) {
+        $scope.formData = {};
+    });
+</script>
+
 </body>
 
 </html>
